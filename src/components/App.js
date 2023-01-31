f55f662bdd1df516a81deadd98652df7d9a0d3c9
Abdul Ragib <abdulragib36@gmail.com>
2023-01-31 13:00:11 +0530

    Solution 2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 const App = ()=>{
     return (
-        <div id='main'>
-        <h1>Hi, I'm a React App</h1>
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <div id="main">
+          <h1>Hi, I'm a React App</h1>
+          <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     );
 }
